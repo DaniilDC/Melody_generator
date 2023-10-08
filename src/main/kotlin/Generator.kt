77adaad0d4204bfd2c40688b683c98314e24e062
Daniil Chernyshov <57674031+DaniilDC@users.noreply.github.com>
2023-10-08 19:18:05 +0300

    Добавлена функция составления мелодии из введённых аккордов.

--- src/main/kotlin/Generator.kt
+++ src/main/kotlin/Generator.kt
@@ -3,10 +3,21 @@ fun main() {
     val chordsInput = readLine()?.split(" ")
 
     if (chordsInput != null && chordsInput.isNotEmpty()) {
-//        val melody = generateMelody(chordsInput)
+        val melody = generateMelody(chordsInput)
         println("Сгенерированная мелодия:")
-//        println(melody)
+        println(melody)
     } else {
         println("Вы не ввели аккорды. Пожалуйста, попробуйте снова.")
     }
+}
+
+fun generateMelody(chords: List<String>): String {
+    val melodyLength = (2..10).random() // случайная длина мелодии от 2 до 10 аккордов
+    val melody = StringBuilder()
+
+    repeat(melodyLength) {
+        val randomChord = chords.random()
+        melody.append("$randomChord ")
+    }
+    return melody.toString().trim()
 }
